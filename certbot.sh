d08d0f8e0f35a14ecf588887b4810e463e4e3e25
Luis Lara <xanderls25@icloud.com>
2023-07-24 11:55:21 -0500

    Subiendo nuevos cambios

--- certbot.sh
+++ certbot.sh
@@ -1,12 +1,10 @@
-# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d mysql.syssoftintegra.com --redirect --no-eff-email
+certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d mysql.syssoftintegra.com --redirect --no-eff-email
 
-# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d next.syssoftintegra.com --redirect --no-eff-email
+certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d laluz.syssoftintegra.com --redirect --no-eff-email
 
-# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d tp.syssoftintegra.com --redirect --no-eff-email
+certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d tp.syssoftintegra.com --redirect --no-eff-email
 
-# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d pj.syssoftintegra.com --redirect --no-eff-email
-
-# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d ferrediamond.ancigame.com --redirect --no-eff-email
+certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d pj.syssoftintegra.com --redirect --no-eff-email
 
 service nginx restart
 
