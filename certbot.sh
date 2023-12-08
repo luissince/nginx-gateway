afdea232cbdf28b1f0f60c10d06a81067e75926f
Luis Lara <xanderls25@icloud.com>
2023-12-08 14:58:09 -0500

    Creando del https a app

--- certbot.sh
+++ certbot.sh
@@ -6,6 +6,8 @@ certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos
 
 certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d pj.syssoftintegra.com --redirect --no-eff-email
 
+certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d app.syssoftintegra.com --redirect --no-eff-email
+
 service nginx restart
 
 tail -f /var/log/nginx/access.log
