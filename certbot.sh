a4cf33d453377b3af623b1772806602b77a41a36
Luis Lara <xanderls25@icloud.com>
2023-12-23 23:50:23 -0500

    Agregando redirect a app_chasquis.conf

--- certbot.sh
+++ certbot.sh
@@ -8,6 +8,8 @@ certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos
 
 certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d app.syssoftintegra.com --redirect --no-eff-email
 
+certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d loschasquis.pe -d www.loschasquis.pe --redirect --no-eff-email
+
 service nginx restart
 
 tail -f /var/log/nginx/access.log
