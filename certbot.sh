cbda73f83713f5a4ba657716bd6911e740f1f80a
Luis Lara <xanderls25@icloud.com>
2024-02-19 17:10:55 -0500

    Creando nuevo certificado

--- certbot.sh
+++ certbot.sh
@@ -16,6 +16,8 @@ certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos
 
 certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d felicitygirls.tiquepos.com --redirect --no-eff-email
 
+certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d bodega-modelo.syssoftintegra.com --redirect --no-eff-email
+
 service nginx restart
 
 tail -f /var/log/nginx/access.log
