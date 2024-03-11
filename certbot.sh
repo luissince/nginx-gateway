70968a4209661b7de9a3ed42324c8e383b812186
Luis Lara <xanderls25@icloud.com>
2024-03-10 20:46:19 -0500

    Crenado nuevo certificado

--- certbot.sh
+++ certbot.sh
@@ -24,6 +24,8 @@ certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos
 
 certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d inmobiliariagmyc.com -d www.inmobiliariagmyc.com --redirect --no-eff-email
 
+certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d sunat.inmobiliariagmyc.com --redirect --no-eff-email
+
 service nginx restart
 
 tail -f /var/log/nginx/access.log
