e5d46d0e352ef7b7238862ae43f3df292350f0cf
Luis Lara <xanderls25@icloud.com>
2024-03-18 07:17:29 -0500

    Creando nuevo servicio

--- certbot.sh
+++ certbot.sh
@@ -26,6 +26,8 @@ certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos
 
 certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d sunat.inmobiliariagmyc.com --redirect --no-eff-email
 
+certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d app.leatsac.com --redirect --no-eff-email
+
 service nginx restart
 
 tail -f /var/log/nginx/access.log
