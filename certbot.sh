395f24aad646fe359ae167ae3892f320f5bbca3f
Luis Lara <xanderls25@icloud.com>
2024-03-26 10:51:44 -0500

    Probando cambios en nuevos servicios

--- certbot.sh
+++ certbot.sh
@@ -1,32 +1,34 @@
-certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d mysql.syssoftintegra.com --redirect --no-eff-email
+# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d mysql.syssoftintegra.com --redirect --no-eff-email
 
 # certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d laluz.syssoftintegra.com --redirect --no-eff-email
 
-certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d tp.syssoftintegra.com --redirect --no-eff-email
+# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d tp.syssoftintegra.com --redirect --no-eff-email
 
 # certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d pj.syssoftintegra.com --redirect --no-eff-email
 
 certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d app.syssoftintegra.com --redirect --no-eff-email
 
-certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d loschasquis.pe -d www.loschasquis.pe --redirect --no-eff-email
+certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d api.syssoftintegra.com --redirect --no-eff-email
 
-certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d legrand.tiquepos.com --redirect --no-eff-email
+# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d loschasquis.pe -d www.loschasquis.pe --redirect --no-eff-email
+
+# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d legrand.tiquepos.com --redirect --no-eff-email
 
 # certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d raices.syssoftintegra.com --redirect --no-eff-email
 
-certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d felicitygirls.tiquepos.com --redirect --no-eff-email
+# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d felicitygirls.tiquepos.com --redirect --no-eff-email
 
 # certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d bodega-modelo.syssoftintegra.com --redirect --no-eff-email
 
-certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d unnagro.syssoftintegra.com --redirect --no-eff-email
+# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d unnagro.syssoftintegra.com --redirect --no-eff-email
 
-certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d syssoftintegra.com -d www.syssoftintegra.com --redirect --no-eff-email
+# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d syssoftintegra.com -d www.syssoftintegra.com --redirect --no-eff-email
 
-certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d inmobiliariagmyc.com -d www.inmobiliariagmyc.com --redirect --no-eff-email
+# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d inmobiliariagmyc.com -d www.inmobiliariagmyc.com --redirect --no-eff-email
 
-certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d sunat.inmobiliariagmyc.com --redirect --no-eff-email
+# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d sunat.inmobiliariagmyc.com --redirect --no-eff-email
 
-certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d app.leatsac.com --redirect --no-eff-email
+# certbot --nginx --keep-until-expiring -m alexander_dx_10@hotmail.com --agree-tos -d app.leatsac.com --redirect --no-eff-email
 
 service nginx restart
 
